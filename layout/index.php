4ac9a146fa6826533b49d5a0047da1f317f9c24a
amjadhamid <majdsy451@gmail.com>
2018-04-21 00:36:57 +0300

    structer and header footer

---
+++ layout/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        {{-- when you install the backeges with npm
+        which it in the package.json
+        you must include it in the project --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://bootswatch.com/4/lux/bootstrap.min.css" rel="stylesheet">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+<body>
+
+
+
+
+
+  </body>
+</html>
